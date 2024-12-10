3be81b69f017548f79d523e392c1ce71b6616778
emmkle01 <147713720+emmkle01@users.noreply.github.com>
2024-12-10 11:03:10 +0100

    animation tabs, ændring af menu og responsive
    Neles kode har vi i kopieret over i min da vi fik merch konflikter

---
+++ edit.js
@@ -0,0 +1,166 @@
+/** Animation når man skifter tilstand tab.
+ * - Hvilken tab blev klikket på?
+ * - hvilken tab kommer vi fra?
+ * - hvilken keyframes animation har vi så brug for?
+ */
+
+
+// få fat i de enkelte tabs og sæt en eventlistener på hver enkelt
+const modesTabs = document.querySelectorAll('.modesTabs__item');
+console.log(modesTabs);
+modesTabs.forEach(tab => {
+    tab.addEventListener('click', changeTab)
+    }
+)  
+
+// få fat i baggrundskassen og finde ud af, hvor den er lige nu 
+const activeTab = document.querySelector('.modesTabs__active');
+
+
+// hvad er den nye left position?
+let activeTab__new = "0"
+
+
+
+function changeTab(tab) {
+    // finde ud af, på hvilken tab brugeren har trykket
+    const target = tab.target;
+    console.log(target);
+
+    // hvilken position har active-kassen på det tidspunkt jeg klikker? 
+    // -> left positionen ligger i en variabel i css, fordi det er meget nemmere at få value og at skift den
+    // SKAL være inde i denne funktion, fordi værdien ellers altid er 0 -> opdaterer sig ikke udenfor funktionen
+    const activePosition = getComputedStyle(document.documentElement).getPropertyValue('--tab__active--left');
+
+    // køre en ny funktion, afhængig af click-event
+    // ny: asleep
+    if (target.classList.contains('modesTabs__item--asleep')){
+
+        changeToAsleep(activePosition);
+    }
+    //ny: atHome
+    if (target.classList.contains('modesTabs__item--atHome')){
+
+        changeToAthome(activePosition);
+    }
+    //ny: away
+    if (target.classList.contains('modesTabs__item--away')){
+
+        changeToAway(activePosition);
+    }
+}
+
+function changeToAsleep(activePosition) {
+    // atHome --> asleep
+    if (activePosition == "0"){
+        activeTab.classList.add('homeToSleep') // for at få den rette transition
+        // tilpas den nye position og overdrage det til css variabel
+        activeTab__new = "33.33%"
+        document.documentElement.style.setProperty('--tab__active--leftNEW', activeTab__new)
+        console.log (getComputedStyle(document.documentElement).getPropertyValue('--tab__active--leftNEW'));
+    
+        // animationen tager 0.5s -> tilpasning af activePosition skal vente, fordi den ellers allerede havde den ny
+        //value inden animationen starter -> ergo: ingen animation
+        setTimeout(() => {
+            document.documentElement.style.setProperty('--tab__active--left', activeTab__new)
+            console.log(activePosition);
+            activeTab.classList.remove('homeToSleep');
+         }, 500)
+    }
+    
+    // away --> asleep
+    if (activePosition == '66.66%'){
+        activeTab.classList.add('awayToSleep') // for at få den rette transition
+    
+        // tilpas den nye position og overdrage det til css variabel
+        activeTab__new = "33.33%"
+        document.documentElement.style.setProperty('--tab__active--leftNEW', activeTab__new)
+        console.log (getComputedStyle(document.documentElement).getPropertyValue('--tab__active--leftNEW'));
+    
+        // animationen tager 1s -> tilpasning af activePosition skal vente, fordi den ellers allerede havde den ny
+        //value inden animationen starter -> ergo: ingen animation
+        setTimeout(() => {
+            document.documentElement.style.setProperty('--tab__active--left', activeTab__new)
+            console.log(activePosition)
+            activeTab.classList.remove('awayToSleep');
+         }, 500)
+    }
+}
+
+function changeToAthome(activePosition) {
+    // asleep --> athome
+    if (activePosition == '33.33%' ){
+        activeTab.classList.add('sleepToHome') // for at få den rette transition
+
+        // tilpas den nye position og overdrage det til css variabel
+        activeTab__new = "0"
+        document.documentElement.style.setProperty('--tab__active--leftNEW', activeTab__new)
+        console.log (getComputedStyle(document.documentElement).getPropertyValue('--tab__active--leftNEW'));
+    
+        // animationen tager 1s -> tilpasning af activePosition skal vente, fordi den ellers allerede havde den ny
+        //value inden animationen starter -> ergo: ingen animation
+        setTimeout(() => {
+            document.documentElement.style.setProperty('--tab__active--left', activeTab__new)
+            console.log(activePosition);
+            activeTab.classList.remove('sleepToHome');
+         }, 500)
+    }
+    
+    // away --> athome
+    if (activePosition == '66.66%'){
+        activeTab.classList.add('awayToHome') // for at få den rette transition
+    
+        // tilpas den nye position og overdrage det til css variabel
+        activeTab__new = "0"
+        document.documentElement.style.setProperty('--tab__active--leftNEW', activeTab__new)
+        console.log (getComputedStyle(document.documentElement).getPropertyValue('--tab__active--leftNEW'));
+    
+        // animationen tager 1s -> tilpasning af activePosition skal vente, fordi den ellers allerede havde den ny
+        //value inden animationen starter -> ergo: ingen animation
+        setTimeout(() => {
+            document.documentElement.style.setProperty('--tab__active--left', activeTab__new)
+            console.log(activePosition);
+            activeTab.classList.remove('awayToHome');
+         }, 500)
+    }
+}
+
+function changeToAway(activePosition) {
+    // asleep --> away
+    
+    if (activePosition == '33.33%'){
+        activeTab.classList.add('sleepToAway') // for at få den rette transition
+
+        // tilpas den nye position og overdrage det til css variabel
+        activeTab__new = "66.66%"
+        document.documentElement.style.setProperty('--tab__active--leftNEW', activeTab__new)
+        console.log (getComputedStyle(document.documentElement).getPropertyValue('--tab__active--leftNEW'));
+    
+        // animationen tager 1s -> tilpasning af activePosition skal vente, fordi den ellers allerede havde den ny
+        //value inden animationen starter -> ergo: ingen animation
+        setTimeout(() => {
+            document.documentElement.style.setProperty('--tab__active--left', activeTab__new)
+            console.log(activePosition);
+            activeTab.classList.remove('sleepToAway');
+         }, 500)
+        
+    }
+    
+    // athome --> away
+    if (activePosition == '0'){
+        activeTab.classList.add('homeToAway') // for at få den rette transition
+    
+        // tilpas den nye position og overdrage det til css variabel
+        activeTab__new = "66.66%"
+        document.documentElement.style.setProperty('--tab__active--leftNEW', activeTab__new)
+        console.log (getComputedStyle(document.documentElement).getPropertyValue('--tab__active--leftNEW'));
+    
+        // animationen tager 1s -> tilpasning af activePosition skal vente, fordi den ellers allerede havde den ny
+        //value inden animationen starter -> ergo: ingen animation
+        setTimeout(() => {
+            document.documentElement.style.setProperty('--tab__active--left', activeTab__new)
+            console.log(activePosition);
+            activeTab.classList.remove('homeToAway');
+         }, 500)
+    }
+}
